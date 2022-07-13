0978ad1042d79d07bd2b35f29b8fc15f87446721
Katherine Parsons <kparsons@arosebooks.com>
2022-07-13 16:04:47 -0400

    adds images

--- src/index.js
+++ src/index.js
@@ -1,11 +1,9 @@
 import React from "react";
 import { createRoot } from "react-dom/client";
-// import "./index.css";
 import 'bootstrap/dist/css/bootstrap.css';
 import 'bootstrap/dist/js/bootstrap';
 import BadBank from './bank';
 import './style.css';
-// import reportWebVitals from "./reportWebVitals";
 
 const rootElement = document.getElementById("root");
 const root = createRoot(rootElement);
