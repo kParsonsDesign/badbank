1d26f9f5a7b64fa5e41b65450d965f67ad2c0449
Katherine Parsons <kparsons@arosebooks.com>
2022-07-14 17:15:55 -0400

    adds deposit function

--- src/deposit.js
+++ src/deposit.js
@@ -1,24 +1,20 @@
-import React from 'react';
-import UserContext, { BankForm } from './context';
+import { CardBootstrap, BankTransactionForm } from './context';
 
 export default function Deposit() {
-  const ctx = React.useContext(UserContext);
-  const user = (ctx.users.find(user => user.loggedIn === true));
-  const userIndex = (ctx.users.findIndex(user => user.loggedIn === true));
-  console.log('loggedIn user:');
-  console.log(user);
-  console.log(userIndex);
-  console.log(ctx);
+
 
   return (
-    <div>
-      <h1 className="my-3">Deposit Page</h1>
-      {JSON.stringify(ctx)}
-      <p className='lead'>Welcome, {user.firstName}. Would you like to deposit money into your account today?</p>
-      <BankForm 
-        header="Deposit"
-        user={userIndex}
+    <>
+      <CardBootstrap 
+        headerText="Deposit"
+        className="mb-5"
+        width="25rem"
+        body={(
+          <>
+          <BankTransactionForm type='deposit' />
+          </>
+        )}
       />
-    </div>
+    </>
   );
 }
